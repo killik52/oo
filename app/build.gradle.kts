2f3fd211077a5f0678cccfc314b7ffe867c2a0a1
killik52 <killik51@gmail.com>
2025-06-15 01:13:28 -0300

    hjg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,10 @@
+// app/build.gradle.kts (Este é o arquivo dentro da pasta 'app')
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
-    id("com.google.devtools.ksp") // Adicione esta linha para o KSP (Kotlin Symbol Processing)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -48,27 +50,25 @@ android {
 }
 
 dependencies {
-    // Dependências do AndroidX e Material Design
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.0.0")) // Kotlin BOM
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     implementation(libs.material)
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation(libs.activity)
 
-    // Dependência Lifecycle ViewModel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
 
-    // Room components
     implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1") // Para coroutines com Room
-    ksp("androidx.room:room-compiler:2.6.1") // Use ksp para o processador de anotação
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 
-    // Retrofit e Gson para APIs Web
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     debugImplementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
-    // Outras dependências do seu projeto
     implementation("com.google.mlkit:text-recognition:16.0.0")
     implementation("io.getstream:photoview:1.0.3")
     implementation("androidx.activity:activity-ktx:1.9.3")
@@ -78,13 +78,9 @@ dependencies {
     implementation("com.google.zxing:core:3.5.3")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 
-    // ViewPager2
     implementation("androidx.viewpager2:viewpager2:1.1.0")
-
-    // MPAndroidChart
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
-    // Testes
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,23 @@
+// build.gradle.kts (Este é o arquivo na RAIZ do seu projeto)
+
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+        classpath("com.android.tools.build:gradle:8.3.0") // Android Gradle Plugin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0") // Plugin Kotlin
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.0-1.0.21") // Plugin KSP
     }
 }
 
-tasks.register<Delete>("clean") {
+plugins {
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+}
+
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
